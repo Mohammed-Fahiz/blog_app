f60c526c03883b7bde7bc269a0377f01ca2c7f78
Mohammed Fahiz <138813355+Mohammed-Fahiz@users.noreply.github.com>
2024-03-24 01:53:45 +0530

    Added error handling files,Sign inm,Sign up screens,Clean architecture,Sign up use case created

---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,29 @@
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/core/usercase/usecase.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/src/either.dart';
+
+class UserSignUp implements UseCase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+  const UserSignUp(this.authRepository);
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String name;
+  final String password;
+  final String email;
+
+  UserSignUpParams({
+    required this.name,
+    required this.password,
+    required this.email,
+  });
+}
--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,35 +1,32 @@
 import 'package:blog_app/core/navigate.dart';
 import 'package:blog_app/core/theme/app_pallete.dart';
-import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
+import 'package:blog_app/features/auth/presentation/pages/signup_page.dart';
 import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
 import 'package:blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
 import 'package:flutter/material.dart';
 
-class SignUpPage extends StatefulWidget {
-  const SignUpPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<SignUpPage> createState() => _SignUpPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _SignUpPageState extends State<SignUpPage> {
+class _LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-  final nameController = TextEditingController();
   final formKey = GlobalKey<FormState>();
 
   @override
   void dispose() {
     emailController.dispose();
     passwordController.dispose();
-    nameController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
@@ -38,15 +35,11 @@ class _SignUpPageState extends State<SignUpPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text(
-                "Sign Up",
+                "Sign In",
                 style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
               ),
               const SizedBox(
                 height: 30,
-              ),
-              AuthField(hintText: "Name", controller: nameController),
-              const SizedBox(
-                height: 15,
               ),
               AuthField(hintText: "Email", controller: emailController),
               const SizedBox(
@@ -60,24 +53,24 @@ class _SignUpPageState extends State<SignUpPage> {
                 height: 20,
               ),
               AuthGradientButton(
-                buttonText: "Sign up",
+                buttonText: "Sign In",
                 onPressed: () {},
               ),
               const SizedBox(height: 20),
               GestureDetector(
                 onTap: () {
-                  Navigate.push(
+                  Navigate.pushRemove(
                     context: context,
-                    screen: const LoginPage(),
+                    screen: const SignUpPage(),
                   );
                 },
                 child: RichText(
                   text: TextSpan(
-                    text: 'Already have an account? ',
+                    text: 'Don\'t have an account? ',
                     style: Theme.of(context).textTheme.titleMedium,
                     children: [
                       TextSpan(
-                        text: 'Sign In',
+                        text: 'Sign Up',
                         style:
                             Theme.of(context).textTheme.titleMedium?.copyWith(
                                   color: AppPallete.gradient2,
